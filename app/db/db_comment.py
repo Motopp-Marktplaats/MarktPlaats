2236b85cc5200bbd184ad7c065886c0b1ea0f45b
Cafer Sonmez <cafer.sonmez@motopp.nl>
2026-01-15 23:05:53 +0100

    Merge branch 'develop' of github.com:Motopp-Marktplaats/MarktPlaats into feature/comments voor conflict:  logout en cooments routers # Conflicts: #	app/main.py

--- app/db/db_comment.py
+++ app/db/db_comment.py
@@ -3,12 +3,12 @@ from app.models.comments import DbComment
 from app.schemas.comments import CommentBase
 from fastapi import HTTPException
 
-
 def create_comment(db: Session, request: CommentBase):
     new_comment = DbComment(
         comment=request.comment,
         create_by_id=request.create_by_id,
         sent_to_id=request.sent_to_id,
+        advertisement_id = request.advertisement_id,
         date=request.date
     )
     db.add(new_comment)
@@ -20,30 +20,24 @@ def read_all_comments(db:Session):
     return db.query(DbComment).all()
 
 def read_comment(db:Session, id: int):
-    return db.query(DbComment).filter(DbComment.comment_id == id).first()
-
+    return db.query(DbComment).filter(DbComment.id == id).first()
 
 def update_comment(db: Session, id: int, request: CommentBase):
-    comment = db.query(DbComment).filter(DbComment.comment_id == id).first()
+    comment = db.query(DbComment).filter(DbComment.id == id).first()
     if not comment:
         raise HTTPException(status_code=404, detail="Comment not found")
     comment.comment = request.comment
-    #comment.create_by_id = request.create_by_id
-    #comment.sent_to_id = request.sent_to_id
-    #comment.date = request.date
 
     db.commit()
     db.refresh(comment)
     return comment
 
-
 def delete_comment(db: Session, id: int):
-    comment = db.query(DbComment).filter(DbComment.comment_id == id).first()
-    db.delete(comment)
-    db.commit()
-    return {"detail": "Comment deleted successfully"}
+    comment = db.query(DbComment).filter(DbComment.id == id).first()
+    try:
+        db.delete(comment)
+        db.commit()
+    except:
+        raise HTTPException(status_code=404, detail="Comment not found")
+    return comment
 
-
-
-# def read_comments_by_ad(db: Session, ad_id: int):
-#     return db.query(DbComment).filter(DbComment.ad_id == ad_id).all()
--- app/schemas/comments.py
+++ app/schemas/comments.py
@@ -2,39 +2,37 @@ from pydantic import BaseModel
 from datetime import date
 
 class CommentBase(BaseModel):
-    #comment_id: int
+    #id: int
     comment: str
     create_by_id: int # current_user = Depends(get_current_user)
     sent_to_id: int
+    advertisement_id: int
     date: date
 
-
 class CommentUpdate(BaseModel):
-    #comment_id: int
+    #id: int
     comment: str
     #create_by_id: int # current_user = Depends(get_current_user)
     #sent_to_id: int
     #date: date
 
-
 class CommentDisplay(BaseModel):
-    comment_id: int
+    id: int
     comment: str
     create_by_id: int
     sent_to_id: int
+    advertisement_id: int
     date: date
     #advertisements: list = []
 
-    class CommentDisplayUpdate(BaseModel):
-        comment_id: int
-        comment: str
-        #create_by_id: int
-        sent_to_id: int
-        date: date
+class CommentDisplayUpdate(BaseModel):
+    #id: int
+    comment: str
+    #create_by_id: int
+    sent_to_id: int
+    date: date
 
     class Config:
         from_attributes = True
 
 
-class CommentDisplayUpdate:
-    pass
