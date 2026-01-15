866fcfb02f8fa7b978023f7982f8e61669836586
Cafer Sonmez <cafer.sonmez@motopp.nl>
2026-01-15 23:06:19 +0100

    Merge branch 'develop' of github.com:Motopp-Marktplaats/MarktPlaats into feature/comments voor conflict:  logout en cooments routers # Conflicts: #	app/main.py

--- app/routers/comments.py
+++ app/routers/comments.py
@@ -5,12 +5,10 @@ from app.db.database import get_db
 from app.db import db_comment
 from typing import List
 
-
 router = APIRouter(
     prefix="/comments",
     tags=["comments"]
 )
-
 #create comment endpoint
 @router.post("/", response_model=CommentDisplay)
 def create_comment(request: CommentBase, db: Session = Depends(get_db)):
@@ -21,18 +19,15 @@ def create_comment(request: CommentBase, db: Session = Depends(get_db)):
 def get_all_comments(db: Session = Depends(get_db)):
     return db_comment.read_all_comments(db)
 
-
 #Read comment endpoint
 @router.get("/{id}", response_model= CommentDisplay)
 def get_comment(id: int, db: Session = Depends(get_db)):
     return db_comment.read_comment(db, id)
 
-
 #Update comment endpoint
 @router.put("/{id}/update")
 def update_comment(id: int, request: CommentUpdate, db: Session = Depends(get_db)):
     return db_comment.update_comment(db, id, request)
-
 
 #Delete comment endpoint
 @router.delete("/{id}/delete")
